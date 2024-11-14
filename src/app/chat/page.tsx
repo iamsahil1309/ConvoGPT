9f6878005be09ce76279be6dcfaf67604fb90745
Sahil Singh <kswayam2000@gmail.com>
2024-11-14 15:42:16 +0530

    sign out

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function ChatPage() {
     return(
         <main className="flex-1">
@@ -8,10 +10,33 @@ export default function ChatPage() {
                             <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
                                 Welcome to Convo-GPT
                             </h1>
-                            <p className="mx-auto max-w-[700px] text-gray-600 md:text-xl dark:text-gray-400">
-                                
+                            <p className="mx-auto max-w-[700px] text-gray-600 md:text-lg dark:text-gray-400">
+                                Convo-GPT is a chatbot that can answer any question you have.
+                                Just
+                                type your question and Convo-GPT respond with an answer.
                             </p>
                         </div>
+                        <div className="w-full max-w-sm space-y-2">
+                            <div className="relative w-full h-48 mt-12 border rounded-lg">
+                                <Image
+                                    src="/xaiLogo.png"
+                                    alt="logo"
+                                    layout="fill"
+                                    className="object-contain aspect-square dark:invert rounded-lg"
+                                    />
+                                    <div className="absolute bottom-0 right-0 left-0 bg-black/90 text-white p-2 rounded-b-lg font-bold uppercase">
+                                        Powered By
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="w-full flex items-center justify-center">
+                <div className="container px-4 md:px-6">
+                    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                        
                     </div>
                 </div>
             </section>
